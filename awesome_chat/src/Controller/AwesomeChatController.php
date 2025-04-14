f8035afacfe5e20ae0c96a8e5f536062a7f15fb5
James Parrott <japarrott@shaw.ca>
2025-04-14 15:32:22 -0600

    routing + controller

---
+++ awesome_chat/src/Controller/AwesomeChatController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\awesome_chat\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Awesome chat routes.
+ */
+final class AwesomeChatController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function __invoke(): array {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
