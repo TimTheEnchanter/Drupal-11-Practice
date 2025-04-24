6125e81c80aa18b9749e191b44794df87fa813d4
James Parrott <japarrott@shaw.ca>
2025-04-24 16:19:33 -0600

    Get the Entity Type Manager Service

---
+++ awesome_chat/src/Controller/NodeCreationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\awesome_chat\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class NodeCreationController extends ControllerBase {
+
+  protected $entityTypeManager;
+
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  public function createMyNode() {
+    
+  }
+
+}
