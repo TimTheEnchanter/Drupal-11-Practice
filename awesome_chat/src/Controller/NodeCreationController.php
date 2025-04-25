a078b011d655cde155846fd91e5ac067ff4d8f1e
James Parrott <japarrott@shaw.ca>
2025-04-25 11:24:11 -0600

    programmatically creating a node of a custom content type

--- awesome_chat/src/Controller/NodeCreationController.php
+++ awesome_chat/src/Controller/NodeCreationController.php
@@ -5,6 +5,8 @@ namespace Drupal\awesome_chat\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class NodeCreationController extends ControllerBase {
 
@@ -21,7 +23,36 @@ class NodeCreationController extends ControllerBase {
   }
 
   public function createMyNode() {
-    
+    $user = $this->entityTypeManager->getStorage('user')->load(1);
+
+    $node = $this->entityTypeManager->getStorage('node')->create([
+      'type' => 'test_content_type',
+      'uid' => $user->id(),
+      'status' => 1,
+      'promote' => 0,
+      'sticky' => 0,
+      'created' => \Drupal::time()->getRequestTime(),
+      'changed' => \Drupal::time()->getRequestTime(),
+    ]);
+
+    $node->setTitle('My Programmatically Created Node at ' . date('Y-m-d H:i:s'));
+    $node->set('body', [
+      'value' => 'This node was created programmatically by the awesome_chat at ' . date('Y-m-d H:i:s') . '.',
+      'format' => 'basic_html',
+    ]);
+
+    try {
+      $node->save();
+      $this->messenger()->addMessage($this->t('Node "@title" (ID: @nid) created successfully.', [
+        '@title' => $node->getTitle(),
+        '@nid' => $node->id(),
+      ]));
+    } catch (\Exception $e) {
+      $this->messenger()->addError($this->t('An error occurred while creating the node: @error', ['@error' => $e->getMessage()]));
+      \Drupal::logger('your_module')->error('Error creating node: @error', ['@error' => $e->getMessage()]);
+    }
+
+    return new RedirectResponse(Url::fromRoute('entity.node.canonical', ['node' => $node->id()])->toString());
   }
 
 }
