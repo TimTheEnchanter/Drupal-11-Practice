03096eb393ddca5a59f86bfb60a4cb9f9fc13e0c
James Parrott <japarrott@shaw.ca>
2025-05-12 16:33:24 -0600

    update routing and setting up form

---
+++ upload-test/src/Form/UploadTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\upload_test\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+/**
+ * Implements the SimpleForm form controller.
+ *
+ * This example demonstrates a simple form with a single text input element. We
+ * extend FormBase which is the simplest form base class used in Drupal.
+ *
+ * @see \Drupal\Core\Form\FormBase
+ */
+class UploadTest extends FormBase {
+
+  public function getFormId() {
+    return 'upload_test_form';
+  }
+  
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['anomalies_file'] = [
+      '#type' => 'file',
+      '#title' =>  $this->t('Valkyrie Anomalies File'),
+      '#required' => TRUE,
+    ];
+
+    $form['patrol_photos'] = [
+      '#type' => 'file',
+      '#title' => $this->t('Patrol Photos'),
+      '#required' => TRUE,
+    ];
+
+    // Group submit handlers in an actions element with a key of "actions" so
+    // that it gets styled correctly, and so that other modules may add actions
+    // to the form. This is not required, but is convention.
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    // Add a submit button that handles the submission of the form.
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+ 
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $title = $form_state->getValue('title');
+    if (strlen($title) < 5) {
+      // Set an error for the form element with a key of "title".
+      $form_state->setErrorByName('title', $this->t('The title must be at least 5 characters long.'));
+    }
+  }
+
+  
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /*
+     * This would normally be replaced by code that actually does something
+     * with the title.
+     */
+    $title = $form_state->getValue('title');
+    $this->messenger()->addMessage($this->t('You specified a title of %title.', ['%title' => $title]));
+  }
+
+}
